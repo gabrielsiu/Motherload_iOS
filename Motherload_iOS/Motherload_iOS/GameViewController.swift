853ebfe691b45b7e99a0a66e41acd144791f3b74
Gabriel Siu <gabrielsiu831@gmail.com>
2023-01-11 11:19:37 -0500

    Initial commit

---
+++ Motherload_iOS/Motherload_iOS/GameViewController.swift
@@ -0,0 +1,46 @@
+//
+//  GameViewController.swift
+//  Motherload_iOS
+//
+//  Created by Gabriel Siu on 2023-01-11.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      view.backgroundColor = .red
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
